b7655f767f25f8379145b94ba1839a80468fc77b
ALAA ELDIN ALHALLAK <alaaeldinalhallak@gmail.com>
2022-02-17 17:18:41 +0300

    Working Fee Category Amount Part 3

--- resources/views/backend/setup/fee_amount/add_fee_amount.blade.php
+++ resources/views/backend/setup/fee_amount/add_fee_amount.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <div class="content-wrapper">
         <div class="container-full">
             <!-- Content Header (Page header) -->
@@ -22,63 +23,69 @@
                                     @csrf
                                     <div class="row">
                                         <div class="col-12">
+                                            <div class="add_item">
 
-                                            <div class="form-group">
-                                                <h5>Fee Category <span class="text-danger">*</span></h5>
-                                                <div class="controls">
-                                                    <select name="fee_category_id" required="" class="form-control">
-                                                        <option value="" selected="" disabled="">Select Fee Category
-                                                        </option>
-                                                        @foreach ($fee_categories as $category)
-                                                            <option value="{{ $category->id }}">{{ $category->name }}
+                                                <div class="form-group">
+                                                    <h5>Fee Category <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <select name="fee_category_id" required="" class="form-control">
+                                                            <option value="" selected="" disabled="">Select Fee Category
                                                             </option>
-                                                        @endforeach
-
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            {{-- end form group --}}
-
-                                            <div class="row">
-                                                <div class="col-md-5">
-
-                                                    <div class="form-group">
-                                                        <h5>Student Class <span class="text-danger">*</span></h5>
-                                                        <div class="controls">
-                                                            <select name="class_id[]" required="" class="form-control">
-                                                                <option value="" selected="" disabled="">Select Fee Category
+                                                            @foreach ($fee_categories as $category)
+                                                                <option value="{{ $category->id }}">
+                                                                    {{ $category->name }}
                                                                 </option>
-                                                                @foreach ($classess as $class)
-                                                                    <option value="{{ $class->id }}">{{ $class->name }}
-                                                                    </option>
-                                                                @endforeach
-                                                            </select>
-                                                        </div>
-                                                    </div> <!-- // end form group -->
+                                                            @endforeach
 
-
-                                                </div> <!-- End col-md-5 -->
-                                                <div class="col-md-5">
-
-                                                    <div class="form-group">
-                                                        <h5>Amount <span class="text-danger">*</span></h5>
-                                                        <div class="controls">
-                                                            <input type="text" name="amount[]" class="form-control">
-                                                        </div>
+                                                        </select>
                                                     </div>
-
-                                                </div><!-- End col-md-5 -->
-
-
-
-                                                <div class="clo-md-2" style="padding-top:25px;">
-                                                    <span class="btn btn-success addeventmore">
-                                                        <li class="fa fa-plus-circle"></li>
-                                                    </span>
                                                 </div>
+                                                {{-- end form group --}}
+
+                                                <div class="row">
+                                                    <div class="col-md-5">
+
+                                                        <div class="form-group">
+                                                            <h5>Student Class <span class="text-danger">*</span></h5>
+                                                            <div class="controls">
+                                                                <select name="class_id[]" required=""
+                                                                    class="form-control">
+                                                                    <option value="" selected="" disabled="">Select Fee
+                                                                        Category
+                                                                    </option>
+                                                                    @foreach ($classess as $class)
+                                                                        <option value="{{ $class->id }}">
+                                                                            {{ $class->name }}
+                                                                        </option>
+                                                                    @endforeach
+                                                                </select>
+                                                            </div>
+                                                        </div> <!-- // end form group -->
+
+
+                                                    </div> <!-- End col-md-5 -->
+                                                    <div class="col-md-5">
+
+                                                        <div class="form-group">
+                                                            <h5>Amount <span class="text-danger">*</span></h5>
+                                                            <div class="controls">
+                                                                <input type="text" name="amount[]" class="form-control">
+                                                            </div>
+                                                        </div>
+
+                                                    </div><!-- End col-md-5 -->
+
+
+
+                                                    <div class="clo-md-2" style="padding-top:25px;">
+                                                        <span class="btn btn-success addeventmore">
+                                                            <li class="fa fa-plus-circle"></li>
+                                                        </span>
+                                                    </div>
+                                                </div>
+
                                             </div>
-
-
+                                            {{-- end add item --}}
 
 
 
@@ -88,6 +95,8 @@
                                             <div class="text-xs-right">
                                                 <input type="submit" class="btn btn-rounded btn-info mb-5" value="Submit">
                                             </div>
+                                        </div>
+                                    </div>
                                 </form>
 
                             </div>
@@ -107,4 +116,62 @@
 
         </div>
     </div>
+    <div style="visibility: hidden;">
+        <div class="whole_extra_item_add" id="whole_extra_item_add">
+            <div class="delete_whole_extra_item_add" id="delete_whole_extra_item_add">
+                <div class="form-row">
+
+                    <div class="col-md-5">
+
+                        <div class="form-group">
+                            <h5>Student Class <span class="text-danger">*</span></h5>
+                            <div class="controls">
+                                <select name="class_id[]" required="" class="form-control">
+                                    <option value="" selected="" disabled="">Select Fee Category</option>
+                                    @foreach ($classess as $class)
+                                        <option value="{{ $class->id }}">{{ $class->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div> <!-- // end form group -->
+                    </div> <!-- End col-md-5 -->
+
+                    <div class="col-md-5">
+
+                        <div class="form-group">
+                            <h5>Amount <span class="text-danger">*</span></h5>
+                            <div class="controls">
+                                <input type="text" name="amount[]" class="form-control">
+                            </div>
+                        </div>
+                    </div><!-- End col-md-5 -->
+
+                    <div class="col-md-2" style="padding-top: 25px;">
+                        <span class="btn btn-success addeventmore"><i class="fa fa-plus-circle"></i> </span>
+                        <span class="btn btn-danger removeeventmore"><i class="fa fa-minus-circle"></i> </span>
+                    </div><!-- End col-md-2 -->
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            var counter = 0;
+            $(document).on("click", ".addeventmore", function() {
+                var whole_extra_item_add = $('#whole_extra_item_add').html();
+                $(this).closest(".add_item").append(whole_extra_item_add);
+                counter++;
+            });
+            $(document).on("click", '.removeeventmore', function(event) {
+                $(this).closest(".delete_whole_extra_item_add").remove();
+                counter -= 1
+            });
+
+        });
+    </script>
 @endsection
