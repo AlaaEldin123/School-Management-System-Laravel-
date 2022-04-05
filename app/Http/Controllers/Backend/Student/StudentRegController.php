541db8453bf20aff26e21642c72ce2fad346c6b4
ALAA ELDIN ALHALLAK <alaaeldinalhallak@gmail.com>
2022-04-05 21:05:15 +0300

    Student PDF Generate Part 1

--- app/Http/Controllers/Backend/Student/StudentRegController.php
+++ app/Http/Controllers/Backend/Student/StudentRegController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Backend\Student;
 
+use PDF;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\AssignStudent;
@@ -260,5 +261,12 @@ class StudentRegController extends Controller
         return redirect()->route('student.registration.view')->with($notification);
     } // End Method
 
+    public function StudentRegDetails($student_id)
+    {
+        $data['details'] = AssignStudent::with(['student', 'discount'])->where('student_id', $student_id)->first();
 
+        $pdf = PDF::loadView('backend.student.student_reg.student_details_pdf', $data);
+        $pdf->SetProtection(['copy', 'print'], '', 'pass');
+        return $pdf->stream('document.pdf');
+    }
 }
